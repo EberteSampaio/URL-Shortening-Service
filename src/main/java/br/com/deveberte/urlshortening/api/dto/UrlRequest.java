7426a080d7035f19ddde8450b5a1c7e92b40ab2a
Eberte Sampaio <deveberte@gmail.com>
2026-08-24 17:04:51 -0300

    feat: incluir mais validações no request

--- src/main/java/br/com/deveberte/urlshortening/api/dto/UrlRequest.java
+++ src/main/java/br/com/deveberte/urlshortening/api/dto/UrlRequest.java
@@ -1,9 +1,14 @@
 package br.com.deveberte.urlshortening.api.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.URL;
 
 public record UrlRequest(
-        @NotBlank(message = "O link é inválido") String link
+        @NotBlank(message = "O link é inválido")
+        @URL(message = "O link deve ser uma URL válida")
+        @Pattern(regexp = "^https?://.+")
+        String link
 ) {
 }
 
