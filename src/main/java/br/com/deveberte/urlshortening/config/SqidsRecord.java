f9c37da614174b51a6194ea3d06d0365bed45a3e
Eberte Sampaio <deveberte@gmail.com>
2026-08-25 08:37:21 -0300

    fix: variable name

--- src/main/java/br/com/deveberte/urlshortening/config/SqidsRecord.java
+++ src/main/java/br/com/deveberte/urlshortening/config/SqidsRecord.java
@@ -3,5 +3,5 @@ package br.com.deveberte.urlshortening.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "app.sqids")
-public record SqidsRecord (int minLenght, String alphabet){
+public record SqidsRecord (int minLength, String alphabet){
 }
